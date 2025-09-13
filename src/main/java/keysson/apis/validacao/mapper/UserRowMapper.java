b101e6cd4ec965a899a2a96dd15824e1e9023721
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-09-13 12:16:55 -0300

    corrigindo mapper

--- src/main/java/keysson/apis/validacao/mapper/UserRowMapper.java
+++ src/main/java/keysson/apis/validacao/mapper/UserRowMapper.java
@@ -20,7 +20,7 @@ public class UserRowMapper implements RowMapper<User> {
         user.setPassword(rs.getString("password"));
         user.setStatus(rs.getInt("status"));
         user.setConsumerId(UUID.fromString(rs.getString("consumer_id")));
-        user.setPrimeiroAcesso(rs.getBoolean("primeiro_acesso"));
+        user.setInitialAccess(rs.getBoolean("primeiro_acesso"));
 
         return user;
     }
